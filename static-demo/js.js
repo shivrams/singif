9d4cd0d2fb698cc6c0550219418e607e3af3a4bc
tobek <tobyfox@gmail.com>
2013-09-28 14:03:52 -0400

    input interface, gif timeline, api request

--- static-demo/js.js
+++ static-demo/js.js
@@ -1,41 +1,74 @@
 "use strict";
 
+var TRANSITION = 250;
+
 $(function() { // upon DOM having loaded
+  $("#singif").click(function() {
+    var button = $(this);
+    if (button.hasClass("pressed")) return;
+    button.addClass("pressed"); // to avoid a flicker when you let go
+    $("#input").delay(TRANSITION/2).animate({ opacity: 0 }, TRANSITION);
+    $("#drivein").css({display: "inline-block"});
+    singif(function singifCB() {
+      button.removeClass("pressed");
+      button.html("let's do it again");
+      $("#input").animate({ opacity: 1 }, TRANSITION);
+    });
+  })
+}); // end upon DOM having loaded
 
-  getSingif("some paramters", function(resp){
+function singif(cb) {
 
+  // reset containers in case we've already played something
+  $("#gifs").html("<div class='gif'></div>");
+  // also reset #yt
+
+  // set loading icon
+  
+  getSingifDummy("some paramters", function getSingifCB(resp){
     if (resp.status != 200) {
       console.error("Something broke. Status code " + resp.status + ": " + resp.mesg);
     }
     else {
       resp.gifs.forEach(function(gif, i) {
         window.setTimeout(function() {
-          $("#drivein").append("<img src='" + gif.url + "'>");
-          // calculate ratio and set CSS width OR height to stretch to fit
-          // very brief crossfade?
-          // delete previous img
-        }, gif.ts*100); // *100 for now cause i don't want to wait 21 seconds
+          $("#gifs").append("<div class='gif' style='background-image: url(" + gif.url + "); display: none;'></div>");
+          $($("#gifs .gif")[1]).fadeIn(TRANSITION);
+          $($("#gifs .gif")[0]).fadeOut(250, function() {
+            $(this).remove();
+          });
+        }, gif.ts*250);
+
+        // and for when we're done:
+        window.setTimeout(function() {
+          cb();
+        }, resp.meta.length*1000);
       });
     }
-
   }); // end getSingif()
-
-}); // end upon DOM having loaded
+}
 
 function getSingif(param1, cb) {
+  $.getJSON("http://singif.com/api/v1/singif", function(data) {
+    cb(data);
+  });
+}
+
+function getSingifDummy(param1, cb) {
   // dummy data:
   cb({
       "status": 200,
       "mesg": "success",
       "meta": {
           "title": "blah",
-          "original_url": "http://whatyoupassedin.com"
+          "original_url": "http://whatyoupassedin.com",
+          "length": 6
       },
       "embed": "<iframe width=\"560\" height=\"315\" src=\"//whatyoupassedin.com\" frameborder=\"0\" allowfullscreen></iframe>",
       "lines": [
           {
               "text": "oh yeah",
-              "ts": 8
+              "ts": 5
           }, {
               "text": "such sweet lyrics",
               "ts": 14
@@ -52,12 +85,12 @@ function getSingif(param1, cb) {
               "style": "center"
           }, {
               "url": "http://media.giphy.com/media/Jztx4VDq2wJBS/giphy.gif",
-              "ts": 14,
+              "ts": 2,
               "loops": 4,
               "style": "center"
           }, {
               "url": "http://media.giphy.com/media/cr5u0ZEbQpVPG/giphy.gif",
-              "ts": 21,
+              "ts": 4,
               "loops": -1,
               "style": "tile"
           }

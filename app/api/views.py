3e78c271c5bd4c7f9a76541f64d1e5a848cf2ea7
Shivram Subramanian <shivram@digg.com>
2013-09-28 17:02:52 -0400

    adding length and id for toby

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,6 @@
 from flask import Response, Blueprint
 import simplejson
-sample_response = {'status': 200, 'gifs': [{'url': 'http://media.giphy.com/media/fiMcQZFenlgFa/giphy.gif', 'loops': 8, 'style': 'center', 'ts': 0}, {'url': 'http://media.giphy.com/media/Jztx4VDq2wJBS/giphy.gif', 'loops': 4, 'style': 'center', 'ts': 14}, {'url': 'http://media.giphy.com/media/cr5u0ZEbQpVPG/giphy.gif', 'loops': -1, 'style': 'tile', 'ts': 21}], 'lines': [{'text': 'oh yeah', 'ts': 8}, {'text': 'such sweet lyrics', 'ts': 14}, {'text': 'time to gif it', 'ts': 21}], 'meta': {'original_url': 'https://www.youtube.com/watch?v=hroUeu4IvpE', 'title': 'blah'}, 'embed': '<iframe width="560" height="315" src="//www.youtube.com/watch?v=hroUeu4IvpE" frameborder="0" allowfullscreen></iframe>', 'mesg': 'success'}
+sample_response = {'status': 200, 'gifs': [{'url': 'http://media.giphy.com/media/fiMcQZFenlgFa/giphy.gif', 'loops': 8, 'style': 'center', 'ts': 0}, {'url': 'http://media.giphy.com/media/Jztx4VDq2wJBS/giphy.gif', 'loops': 4, 'style': 'center', 'ts': 14}, {'url': 'http://media.giphy.com/media/cr5u0ZEbQpVPG/giphy.gif', 'loops': -1, 'style': 'tile', 'ts': 21}], 'lines': [{'text': 'oh yeah', 'ts': 8}, {'text': 'such sweet lyrics', 'ts': 14}, {'text': 'time to gif it', 'ts': 21}], 'meta': {'original_url': 'https://www.youtube.com/watch?v=hroUeu4IvpE', 'id': 'hroUeu4IvpE', 'embed_type': 'youtube', 'internal_id': '12151', 'title': 'blah', 'artist': 'WASTE', 'length': 27}, 'mesg': 'success'}
 
 mod = Blueprint('api', __name__)
 
